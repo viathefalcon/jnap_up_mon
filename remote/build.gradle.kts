cef3cd2c2303c382431ea565004af5e6a1e732bd
Stephen Higgins <stephen.higgins@outlook.com>
2026-02-04 01:07:42 +0000

    Fix build errors

--- remote/build.gradle.kts
+++ remote/build.gradle.kts
@@ -1,15 +1,6 @@
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
-buildscript {
-    repositories {
-        google()
-        mavenCentral()
-    }
-    dependencies {
-        classpath("com.android.tools.build:gradle:8.1.0")
-        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.9.0")
-    }
-}
-
 plugins {
-    id("org.jetbrains.kotlin.plugin.compose") version "1.9.0" apply false
+    alias(libs.plugins.android.application) apply false
+    alias(libs.plugins.kotlin.android) apply false
+    alias(libs.plugins.kotlin.compose) apply false
 }
